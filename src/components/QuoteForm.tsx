d80e795533d6a8ad8bb9ab524ee12f1b73128b6d
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-02 08:02:21 +0300

    added an add quote function in quoteform

--- src/components/QuoteForm.tsx
+++ src/components/QuoteForm.tsx
@@ -1,20 +1,56 @@
+import { useState } from "react";
+
+type Quote = {
+    quote: string;
+    author: string;
+  };
+
 const QuoteForm = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [quoteData, setQuoteData] = useState<{ quote: string; author: string }[]>([]);
+
+
+  const handleAddQuote = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newQuote = {
+        quote: quote,
+        author: author,
+      };
+
+      setQuoteData(prev => [...prev, newQuote])
+
+      setQuote("");
+      setAuthor("");
+    
+ }
+
   return (
-    <div className="bg-white p-5 rounded shadow">
+    <form className="bg-white p-5 rounded shadow">
       <div className="flex flex-col gap-3">
         <input
           type="text"
           placeholder="Quote text"
+          value={quote}
+          onChange={(e) => setQuote(e.target.value)}
           className="border border-gray-300 bg-white rounded px-2 text-gray-500"
         />
         <input
           type="text"
           placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
           className="border border-gray-300 bg-white rounded px-2 text-gray-500"
         />
       </div>
-      <button type="submit" className="bg-blue-600 text-white px-2 my-2 w-full rounded">Add Quote</button>
-    </div>
+      <button
+        type="submit"
+        onClick={handleAddQuote}
+        className="bg-blue-600 text-white px-2 my-2 w-full rounded  cursor-pointer hover:bg-blue-700"
+      >
+        Add Quote
+      </button>
+    </form>
   );
 };
 
